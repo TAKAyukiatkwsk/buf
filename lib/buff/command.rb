b5eeb14bfcf0071ed0e1ca52271d843ace65ce56
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2018-12-11 23:37:13 +0900

    Refactor variables about template

--- lib/buff/command.rb
+++ lib/buff/command.rb
@@ -7,13 +7,18 @@ module Buff
   class Command < Thor
     include Thor::Actions
 
+    def initialize(*args)
+      super
+
+      @template_dir_name = "templates"
+      @template_ext = ".mustache"
+      @template_dir = File.expand_path("../../#{@template_dir_name}", File.dirname(__FILE__))
+    end
+
     desc 'list', 'List templates'
     def list
-      template_dir_name = "templates"
-      template_ext = ".mustache"
-      template_dir = File.expand_path("../../#{template_dir_name}", File.dirname(__FILE__))
-      templates = Dir.glob("#{template_dir}/*#{template_ext}")
-        .map {|f| File.basename(f, template_ext) }
+      templates = Dir.glob("#{@template_dir}/*#{@template_ext}")
+        .map {|f| File.basename(f, @template_ext) }
 
       if templates.empty?
         say("There is no template files in templates directory.", color = :red)
@@ -26,10 +31,7 @@ module Buff
     method_options :d => :string, :args => :array
     def post(template)
       # check template
-      template_dir_name = "templates"
-      template_ext = ".mustache"
-      template_dir = File.expand_path("../../#{template_dir_name}", File.dirname(__FILE__))
-      template_file = "#{template_dir}/#{template}#{template_ext}"
+      template_file = "#{@template_dir}/#{template}#{@template_ext}"
       unless File.exist?(template_file)
         say("Invalid template name: #{template}. Please check template directory.", color = :red)
         exit 1
