faa3ef09f101234cc28c97a2a9bd2efec61a4d73
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2018-12-12 16:54:59 +0900

    Implement post command
    - checking template file

--- lib/buff/command.rb
+++ lib/buff/command.rb
@@ -18,5 +18,20 @@ module Buff
         templates.each {|t| say t }
       end
     end
+
+    desc 'post', 'Post to Buffer'
+    method_options :d => :string, :args => :array
+    def post(template)
+      # check template
+      template_dir_name = "templates"
+      template_ext = ".mustache"
+      template_dir = File.expand_path("../../#{template_dir_name}", File.dirname(__FILE__))
+      unless File.exist?("#{template_dir}/#{template}#{template_ext}")
+        say("Invalid template name: #{template}. Please check template directory.", color = :red)
+        exit 1
+      end
+      # apply args to template and generate body
+      # request Buffer API
+    end
   end
 end
